9ea66ee3a7a73f2dd2293b97684414ea4f909a17
Гурин Ярослав Игоревич <gurin.ucheba@gmail.com>
2023-12-16 21:07:26 +0500

    fixes

--- src/utils/Block.ts
+++ src/utils/Block.ts
@@ -1,13 +1,13 @@
-import { nanoid } from 'nanoid';
-import { EventBus } from './EventBus';
+import { nanoid } from "nanoid";
+import { EventBus } from "./EventBus";
 
 // Нельзя создавать экземпляр данного класса
 class Block {
   static EVENTS = {
-    INIT: 'init',
-    FLOW_CDM: 'flow:component-did-mount',
-    FLOW_CDU: 'flow:component-did-update',
-    FLOW_RENDER: 'flow:render',
+    INIT: "init",
+    FLOW_CDM: "flow:component-did-mount",
+    FLOW_CDU: "flow:component-did-update",
+    FLOW_RENDER: "flow:render",
   };
 
   public id = nanoid(6);
@@ -22,16 +22,10 @@ class Block {
 
   private _element: HTMLElement | null = null;
 
-  private _meta: { props: any; };
-
   constructor(propsWithChildren: any = {}) {
     const eventBus = new EventBus();
 
     const { props, children } = this._getChildrenAndProps(propsWithChildren);
-
-    this._meta = {
-      props,
-    };
 
     this.children = children;
     this.props = this._makePropsProxy(props);
@@ -60,9 +54,12 @@ class Block {
 
   _addEvents() {
     const { events = {} } = this.props as { events: Record<string, () => void> };
-    console.log(events);
-    Object.keys(events).forEach((eventName) => {
-      this._element?.addEventListener(eventName, events[eventName]);
+    const objectKeys: string[] = Object.keys(events)
+    objectKeys.forEach(eventName  => {
+      if (this._element){
+        // @ts-ignore
+        this._element.addEventListener(eventName, events[eventName]);
+      }
     });
   }
 
@@ -79,20 +76,20 @@ class Block {
     this.eventBus().emit(Block.EVENTS.FLOW_RENDER);
   }
 
-  protected init() {
-  }
+  protected init() {}
 
   _componentDidMount() {
     this.componentDidMount();
   }
 
-  componentDidMount() {
-  }
+  componentDidMount() {}
 
   public dispatchComponentDidMount() {
     this.eventBus().emit(Block.EVENTS.FLOW_CDM);
 
-    Object.values(this.children).forEach((child) => child.dispatchComponentDidMount());
+    Object.values(this.children).forEach((child) =>
+      child.dispatchComponentDidMount(),
+    );
   }
 
   private _componentDidUpdate(oldProps: any, newProps: any) {
@@ -102,7 +99,8 @@ class Block {
   }
 
   protected componentDidUpdate(oldProps: any, newProps: any) {
-    return true;
+
+    return JSON.stringify(oldProps) === JSON.stringify(newProps);
   }
 
   setProps = (nextProps: any) => {
@@ -136,7 +134,7 @@ class Block {
 
     const html = template(contextAndStubs);
 
-    const temp = document.createElement('template');
+    const temp = document.createElement("template");
 
     temp.innerHTML = html;
 
@@ -162,7 +160,7 @@ class Block {
     return new Proxy(props, {
       get(target, prop) {
         const value = target[prop];
-        return typeof value === 'function' ? value.bind(target) : value;
+        return typeof value === "function" ? value.bind(target) : value;
       },
       set(target, prop, value) {
         const oldTarget = { ...target };
@@ -175,7 +173,7 @@ class Block {
         return true;
       },
       deleteProperty() {
-        throw new Error('Нет доступа');
+        throw new Error("Нет доступа");
       },
     });
   }
@@ -186,11 +184,11 @@ class Block {
   }
 
   show() {
-    this.getContent()!.style.display = 'block';
+    this.getContent()!.style.display = "block";
   }
 
   hide() {
-    this.getContent()!.style.display = 'none';
+    this.getContent()!.style.display = "none";
   }
 }
 
