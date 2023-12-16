e9eabc39484b0321071d916cd452985f2e621288
Гурин Ярослав Игоревич <gurin.ucheba@gmail.com>
2023-12-16 21:17:39 +0500

    fix

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// const PORT = 3000;
+const PORT = 3000;
 
 const app = express();
 
-
 app.use(express.static(`${__dirname}/dist`));
 
+app.listen(PORT, () => {});
